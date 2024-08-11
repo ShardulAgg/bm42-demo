023a64ce41bf1f48c9db3befb92bc2d62f46d04e
ShardulAgg <60056793+ShardulAgg@users.noreply.github.com>
2024-08-10 20:12:11 -0700

    updated backend endpoints

--- backend/api.py
+++ backend/api.py
@@ -1,10 +1,19 @@
 from core import *
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
 
 app_router = APIRouter()
 
 @app_router.get("/")
 def health_check():
     return {"status": "ok"}
+
+@app_router.post("/upload_references")
+async def file_upload(file: UploadFile = File(...)):
+    pass
+
+@app_router.post("/search")
+async def search(query: str):
+    pass
+
 
 __all__ = ["app_router"]
